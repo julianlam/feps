0bcbce25de2dc01e5e8b024284b6451414bd32ef
silverpill <silverpill@firemail.cc>
2026-06-27 18:00:06 +0200

    Fix test_clean_markdown failure (#869)
    Fixes test failure introduced in https://codeberg.org/fediverse/fep/pulls/868
    
    Reviewed-on: https://codeberg.org/fediverse/fep/pulls/869

--- scripts/fep_tools/test_site_fep_page.py
+++ scripts/fep_tools/test_site_fep_page.py
@@ -37,7 +37,7 @@ def test_format_metatable():
         ("[link](http://test.example)", "link"),
         ("[link][link]", "link"),
         ("[a][a] [b][b]", "a b"),
-        ('a "b"', r'a \"b\"'),
+        ('a "b"', r'a &quot;b&quot;'),
     ],
 )
 def test_clean_markdown(text: str, expected: str):
